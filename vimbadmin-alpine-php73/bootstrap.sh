c1172cfbc66dd15c350e4030f30b638170f89b9d
Thijs Eilander <eilander@myguard.nl>
2020-04-30 22:25:23 +0200

    fixes

--- vimbadmin-alpine-php73/bootstrap.sh
+++ vimbadmin-alpine-php73/bootstrap.sh
@@ -10,9 +10,12 @@ if [ ! -f ${FIRSTRUN} ]; then
     cp ${WORKDIR}/application/configs/application.ini.dist ${WORKDIR}/application/configs/application.ini
     echo "[docker : production]" >> ${WORKDIR}/application/configs/application.ini
 fi
+    chown -R root:root ${WORKDIR}/application/configs/
     chown -R root:root ${WORKDIR}/
     chown -R apache:apache ${WORKDIR}/var
-
+    mkdir -p /tmp
+    chmod 1777 -R /tmp 
+ 
    echo "nameserver ${NAMESERVER}" > /etc/resolv.conf
 
 
