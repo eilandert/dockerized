9a3a232132989c430ce2fb5bd1bf6cad3e43a3a0
Thijs Eilander <eilander@myguard.nl>
2020-05-03 01:19:03 +0200

    cleaning up

--- clamav-unofficial-signatures/bootstrap.sh
+++ clamav-unofficial-signatures/bootstrap.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
 set -eu
 
-  if [ -z ${NAMESERVER} ]; then
-   echo "nameserver ${NAMESERVER}" > /etc/resolv.conf
-  fi
-
 # If there are no configfiles, copy them
-    FIRSTRUN="/config/clamav/clamd.conf"
-    if [ ! -f ${FIRSTRUN} ]; then
-      echo "[bootstrap] no configs found, copying..."
-      mkdir -p /config \
-      && cp -r config.orig/* /config/
-    fi
-    rm -rf /etc/clamav \
-      && ln -s /config/clamav /etc/clamav
-    rm -rf /etc/clamav-unofficial-sigs \
-      && ln -s /config/clamav-unofficial-sigs/ /etc/clamav-unofficial-sigs
-    chmod 777 /dev/stdout
+	FIRSTRUN="/config/clamav/clamd.conf"
+	if [ ! -f ${FIRSTRUN} ]; then
+	  echo "[BOOTSTRAP] no configs found, copying..."
+	  mkdir -p /config \
+	  && cp -r config.orig/* /config/
+	fi
 
-    while [ 1 ]; do /usr/local/sbin/clamav-unofficial-sigs -s; sleep 3683; done &
+# Make symlinks to /config so we can bind that
+	rm -rf /etc/clamav && ln -s /config/clamav /etc/clamav
+	rm -rf /etc/clamav-unofficial-sigs && ln -s /config/clamav-unofficial-sigs/ /etc/clamav-unofficial-sigs
+
+	chmod 777 /dev/stdout
 
 #Are there signatures?
-    CVD_FILE="/var/lib/clamav/main.cvd"
-    if [ ! -f ${CVD_FILE} ]; then
-      echo "[bootstrap] No signatures found, running updater"
-      freshclam --user=clamav --no-warnings --foreground
-    fi
+	CVD_FILE="/var/lib/clamav/main.cvd"
+	if [ ! -f ${CVD_FILE} ]; then
+	  echo "[BOOTSTRAP] main.cvd not found found, running updaters"
+          /usr/local/sbin/clamav-unofficial-sigs -s &
+	  freshclam --user=clamav --no-warnings --foreground
+	fi
 
-    freshclam -d -c24  --user=clamav
+	#poor mans cron
+	while [ 1 ]; do sleep 3683; /usr/local/sbin/clamav-unofficial-sigs -s; done &
+	freshclam -d -c24 --user=clamav
 
 exec   /usr/sbin/clamd
 
