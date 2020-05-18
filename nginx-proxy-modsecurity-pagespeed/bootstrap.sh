cb5b01ef097384be3f06fbeb9177aea263276d5d
Thijs Eilander <eilander@myguard.nl>
2020-05-18 23:42:18 +0200

    typo

--- nginx-proxy-modsecurity-pagespeed/bootstrap.sh
+++ nginx-proxy-modsecurity-pagespeed/bootstrap.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 
-        echo "[BOOTSTRAP] This docker image can be found on"
-        echo "[BOOTSTRAP] https://hub.docker.com/u/eilandert or https://github.com/eilandert/dockerized"
-        echo "[BOOTSTRAP]"
-        echo "[BOOTSTRAP] optional variables for this container:"
-        echo "[BOOTSTRAP] NAMESERVER"
+        echo "[BOOTSTRAP] This docker image can be found on https://hub.docker.com/u/eilandert or https://github.com/eilandert/dockerized"
 
         #set nameserver if variable is set
         if [ -n "${NAMESERVER}" ]; then
                 echo "nameserver ${NAMESERVER}" > /etc/resolv.conf
-                echo "[BOOTSTRAP] wait for nameserver to be up with timeout of 60 secs"
-                ping -c1 -W60 ${NAMESERVER}
         fi
 
 	# If there are no configfiles, copy them
@@ -23,15 +17,6 @@
 
         chmod 777 /dev/stdout
 
-
-echo ""
-echo "-----------------------------------------------------------------"
-echo "  For more info see:                                             "
-echo "  https://launchpad.net/~eilander/+archive/ubuntu/nginx          "
-echo "  https://github.com/eilandert/dockerized                        "
-echo "  https://hub.docker.com/r/eilandert/nginx-modsecurity3-pagespeed"
-echo "-----------------------------------------------------------------"
-echo ""
-echo "[BOOTSTRAP] starting NGINX, no more output here"
+	echo "[BOOTSTRAP] starting NGINX, no more output here"
 
 exec nginx -g 'daemon off;'
