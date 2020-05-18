635b587d93b8f9285b234930a4ac36db39276f1a
Thijs Eilander <eilander@myguard.nl>
2020-05-18 23:42:32 +0200

    first commit

---
+++ nginx-alpine/README.md
@@ -0,0 +1,6 @@
+
+dockerized nginx based on alpine linux
+nothing more, nothing less. used to serve some static content
+
+bind /etc/nginx to your local dir and it will be populated
+
---
+++ nginx-alpine/bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+        echo "[BOOTSTRAP] This docker image can be found on https://hub.docker.com/u/eilandert or https://github.com/eilandert/dockerized"
+
+        #set nameserver if variable is set
+        if [ -n "${NAMESERVER}" ]; then
+                echo "nameserver ${NAMESERVER}" > /etc/resolv.conf
+        fi
+
+	# If there are no configfiles, copy them
+	FIRSTRUN="/etc/nginx/nginx.conf"
+	if [ ! -f ${FIRSTRUN} ]; then
+          echo "[BOOTSTRAP] no configs found, populating default configs to /etc/nginx and /etc/modsecurity"
+          cp -r /etc/nginx.orig/* /etc/nginx/
+        fi
+
+        chmod 777 /dev/stdout
+        mkdir -p /run/nginx
+	chown nginx:nginx /run/nginx
+
+	echo "[BOOTSTRAP] starting NGINX, no more output here"
+
+exec nginx -g 'daemon off;'
