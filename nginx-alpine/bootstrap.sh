6b8729cbaf629efd6d75fcc9f0627a382d045258
Thijs Eilander <eilander@myguard.nl>
2020-05-28 04:25:00 +0200

    change [BOOTSTRAP] to [NGINX-ALPINE]

--- nginx-alpine/bootstrap.sh
+++ nginx-alpine/bootstrap.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-        echo "[BOOTSTRAP] This docker image can be found on https://hub.docker.com/u/eilandert or https://github.com/eilandert/dockerized"
+        echo "[NGINX-ALPINE] This docker image can be found on https://hub.docker.com/u/eilandert or https://github.com/eilandert/dockerized"
 
 	# If there are no configfiles, copy them
 	FIRSTRUN="/etc/nginx/nginx.conf"
 	if [ ! -f ${FIRSTRUN} ]; then
-          echo "[BOOTSTRAP] no configs found, populating default configs to /etc/nginx and /etc/modsecurity"
+          echo "[NGINX-ALPINE] no configs found, populating default configs to /etc/nginx and /etc/modsecurity"
           cp -r /etc/nginx.orig/* /etc/nginx/
         fi
 
@@ -13,6 +13,6 @@
         mkdir -p /run/nginx
 	chown nginx:nginx /run/nginx
 
-	echo "[BOOTSTRAP] starting NGINX, no more output here"
+	echo "[NGINX-ALPINE] starting NGINX, no more output here"
 
 exec nginx -g 'daemon off;'
