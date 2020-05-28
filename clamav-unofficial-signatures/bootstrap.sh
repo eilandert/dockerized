4abb144e47207a222cebba18680a4d9b1186e786
Thijs Eilander <eilander@myguard.nl>
2020-05-28 04:20:06 +0200

    change [BOOTSTRAP] to [CLAMAV], remove nameserver variable

--- clamav-unofficial-signatures/bootstrap.sh
+++ clamav-unofficial-signatures/bootstrap.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-	echo "[BOOTSTRAP] This docker image can be found on https://hub.docker.com/u/eilandert / https://github.com/eilandert/dockerized"
-
-	#set nameserver if variable is set.  (but please use dns: in dockercompose).
-	if [ -n "${NAMESERVER}" ]; then
-		echo "nameserver ${NAMESERVER}" > /etc/resolv.conf
-	fi
+	echo "[CLAMAV] This docker image can be found on https://hub.docker.com/u/eilandert / https://github.com/eilandert/dockerized"
 
 # If there are no configfiles, copy them
 	FIRSTRUN="/config/clamav/clamd.conf"
 	if [ ! -f ${FIRSTRUN} ]; then
-	  echo "[BOOTSTRAP] clamd.conf not found, populating default configs to /config"
+	  echo "[CLAMAV] clamd.conf not found, populating default configs to /config"
 	  mkdir -p /config \
 	  && cp -r config.orig/* /config/
 	fi
@@ -25,19 +20,19 @@
 #Are there signatures?
 	CVD_FILE="/var/lib/clamav/main.cvd"
 	if [ ! -f ${CVD_FILE} ]; then
-	  echo "[BOOTSTRAP] main.cvd not found, running clamav-unofficial-sigs in background"
-          echo "[BOOTSTRAP] waiting for internet to be up, pinging 8.8.8.8 with timeout of 60 secs"
+	  echo "[CLAMAV] main.cvd not found, running clamav-unofficial-sigs in background"
+          echo "[CLAMAV] waiting for internet to be up, pinging 8.8.8.8 with timeout of 60 secs"
           ping -c1 -W60 8.8.8.8
           /usr/local/sbin/clamav-unofficial-sigs -s &
-	  echo "[BOOTSTRAP] Running Freshclam in foreground once"
+	  echo "[CLAMAV] Running Freshclam in foreground once"
 	  freshclam --user=clamav --no-warnings --foreground
 	fi
 
 	#poor mans cron
-        echo "[BOOTSTRAP] Starting updaters in the background"
+        echo "[CLAMAV] Starting updaters in the background"
 	while [ 1 ]; do /usr/local/sbin/clamav-unofficial-sigs -s; sleep 3661; done &
 	freshclam -d -c6 --user=clamav
 
-	 echo "[BOOTSTRAP] Starting clamd... Please wait while loading databases"
+	 echo "[CLAMAV] Starting clamd... Please wait while loading databases"
 
 exec   /usr/sbin/clamd
