8085ad1590374b40e6379d86100fe99c88ac9522
Thijs Eilander <eilander@myguard.nl>
2020-05-28 04:23:12 +0200

    change [BOOTSTRAP] to [DOVECOT], remove nameserver variable

--- dovecot/bootstrap.sh
+++ dovecot/bootstrap.sh
@@ -1,21 +1,10 @@
 #!/bin/sh
 
-        echo "[BOOTSTRAP] This docker image can be found on"
-        echo "[BOOTSTRAP] https://hub.docker.com/u/eilandert or https://github.com/eilandert/dockerized"
-        echo "[BOOTSTRAP]"
-        echo "[BOOTSTRAP] optional variables for this container:"
-        echo "[BOOTSTRAP] NAMESERVER"
-
-        #set nameserver if variable is set
-        if [ -n "${NAMESERVER}" ]; then
-                echo "nameserver ${NAMESERVER}" > /etc/resolv.conf
-                echo "[BOOTSTRAP] wait for nameserver to be up with timeout of 60 secs"
-                ping -c1 -W60 ${NAMESERVER}
-        fi
+        echo "[DOVECOT] This docker image can be found on https://hub.docker.com/u/eilandert and https://github.com/eilandert/dockerized"
 
 	FIRSTRUN="/etc/dovecot/dovecot.conf"
 	if [ ! -f ${FIRSTRUN} ]; then
-	  echo "[bootstrap] no configs found, copying..."
+	  echo "[DOVECOT] no configs found, copying..."
 	  mkdir -p /etc/dovecot && cp -r /etc/dovecot.orig/* /etc/dovecot/
 	  sed -i s/"\#log_path\ \=\ syslog"/"log_path\ \=\ \/dev\/stdout"/ /etc/dovecot/conf.d/10-logging.conf
 	fi
