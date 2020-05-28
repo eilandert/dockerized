a37c8644d5095df25f19fbc29c67fa2f723e5216
Thijs Eilander <eilander@myguard.nl>
2020-05-28 04:27:03 +0200

    remove nameserver variable

--- postfix/bootstrap.sh
+++ postfix/bootstrap.sh
@@ -2,13 +2,6 @@
 
         echo "[POSTFIX] This docker image can be found on https://hub.docker.com/u/eilandert and https://github.com/eilandert/dockerized"
 
-        #set nameserver if variable is set
-        if [ -n "${NAMESERVER}" ]; then
-                echo "nameserver ${NAMESERVER}" > /etc/resolv.conf
-                echo "[POSTFIX] wait for nameserver to be up with timeout of 60 secs"
-                ping -c1 -W60 ${NAMESERVER}
-        fi
-
 	FIRSTRUN="/etc/postfix/main.cf"
         if [ ! -f ${FIRSTRUN} ]; then
           echo "[POSTFIX] main.cf not found, populating default configs to /etc/postfix"
