e05bf20acaad1818974e006ca3a9ebcea4461e41
Thijs Eilander <eilander@myguard.nl>
2020-05-28 04:26:24 +0200

    change [BOOTSTRAP] to [POSTFIX]

--- postfix/bootstrap.sh
+++ postfix/bootstrap.sh
@@ -1,21 +1,17 @@
 #!/bin/sh
 
-        echo "[BOOTSTRAP] This docker image can be found on"
-        echo "[BOOTSTRAP] https://hub.docker.com/u/eilandert or https://github.com/eilandert/dockerized"
-        echo "[BOOTSTRAP]"
-        echo "[BOOTSTRAP] optional variables for this container:"
-        echo "[BOOTSTRAP] NAMESERVER"
+        echo "[POSTFIX] This docker image can be found on https://hub.docker.com/u/eilandert and https://github.com/eilandert/dockerized"
 
         #set nameserver if variable is set
         if [ -n "${NAMESERVER}" ]; then
                 echo "nameserver ${NAMESERVER}" > /etc/resolv.conf
-                echo "[BOOTSTRAP] wait for nameserver to be up with timeout of 60 secs"
+                echo "[POSTFIX] wait for nameserver to be up with timeout of 60 secs"
                 ping -c1 -W60 ${NAMESERVER}
         fi
 
 	FIRSTRUN="/etc/postfix/main.cf"
         if [ ! -f ${FIRSTRUN} ]; then
-          echo "[BOOTSTRAP] main.cf not found, populating default configs to /etc/postfix"
+          echo "[POSTFIX] main.cf not found, populating default configs to /etc/postfix"
 	  mkdir -p /etc/postfix \
           && cp -r /etc/postfix.orig/* /etc/postfix/
         fi
