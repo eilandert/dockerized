b106b354bebc17353ccd5d20d994da5091493d54
Thijs Eilander <eilander@myguard.nl>
2020-09-23 01:53:28 +0200

    added support for variable TZ (timezone)

--- letsencrypt/bootstrap.sh
+++ letsencrypt/bootstrap.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
+        echo "[LETSENCRYTPT] This docker image can be found on https://hub.docker.com/u/eilandert or https://github.com/eilandert/dockerized"
+
+
+        if [ -n "${TZ}" ]; then
+         rm /etc/timezone /etc/localtime
+         echo "${TZ}" > /etc/timezone
+         ln -s /usr/share/zoneinfo/${TZ} /etc/localtime
+        fi
+
+
+
 while (true)  do certbot renew; sleep 1d; done
 
