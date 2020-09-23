3c22629b68e928cc6e2928f4bfadc2143ecae8ff
Thijs Eilander <eilander@myguard.nl>
2020-09-23 16:57:31 +0200

    first commit

---
+++ apache-phpfpm/README.md
@@ -0,0 +1,6 @@
+
+Apache + PHP (packages from https://launchpad.net/~ondrej)
+A docker I created for serving wordpress behind a nginx proxy with mod-security and pagespeed. But I guess you could do anything with it. It includes nullmailer.
+See docker-compose.yml for examples.
+
+I am open for suggestions.
---
+++ apache-phpfpm/bootstrap.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+        echo "[APACHE-WP] This docker image can be found on https://hub.docker.com/u/eilandert or https://github.com/eilandert/dockerized"
+
+        if [ -n "${TZ}" ]; then
+         rm /etc/timezone /etc/localtime
+         echo "${TZ}" > /etc/timezone
+         ln -s /usr/share/zoneinfo/${TZ} /etc/localtime
+        fi
+
+
+        # If there are no configfiles, copy them
+        FIRSTRUN="/etc/apache2/apache2.conf"
+        if [ ! -f ${FIRSTRUN} ]; then
+          echo "[APACHE-WP] no configs found, populating default configs to /etc/apache2"
+          cp -r /etc/apache2.orig/* /etc/apache2/
+        fi
+
+        FIRSTRUN="/etc/php/7.4/fpm/php-fpm.conf"
+        if [ ! -f ${FIRSTRUN} ]; then
+          echo "[APACHE-WP] no configs found, populating default configs to /etc/php"
+          cp -r /etc/php.orig/* /etc/php/
+        fi
+
+        FIRSTRUN="/etc/nullmailer/defaultdomain"
+        if [ ! -f ${FIRSTRUN} ]; then
+          echo "[APACHE-WP] no configs found, populating default configs to /etc/nullmailer"
+          cp -r /etc/nullmailer.orig/* /etc/nullmailer
+        fi
+
+        mkdir -p /run/php
+        chown www-data:www-data /run/php
+        chmod 755 /run/php
+
+        service php7.4-fpm restart
+        /usr/sbin/nullmailer-send 1>/var/log/nullmailer.log 2>&1 &
+
+        chmod 777 /dev/stdout
+
+exec /usr/sbin/apache2ctl -DFOREGROUND
+
