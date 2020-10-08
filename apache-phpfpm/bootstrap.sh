6442a29f15ed2fc461bce479751cf209eb83e374
Thijs Eilander <eilander@myguard.nl>
2020-10-08 08:13:21 +0200

    fix rm

--- apache-phpfpm/bootstrap.sh
+++ apache-phpfpm/bootstrap.sh
@@ -37,7 +37,7 @@
         service php7.4-fpm restart 1>/dev/null 2>&1
 
 	#fix some weird issue with nullmailer
-	rm -r /var/spool/nullmailer/trigger
+	rm -f /var/spool/nullmailer/trigger
 	/usr/bin/mkfifo /var/spool/nullmailer/trigger
 	/bin/chmod 0622 /var/spool/nullmailer/trigger
 	/bin/chown -R mail:mail /var/spool/nullmailer/ /etc/nullmailer 
