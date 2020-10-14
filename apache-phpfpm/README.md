d2f5ae6b1a29b02250348b938a924ebdc9739677
Thijs Eilander <eilander@myguard.nl>
2020-10-15 00:38:18 +0200

    added multimode to enable running multiple versions

--- apache-phpfpm/README.md
+++ apache-phpfpm/README.md
@@ -12,22 +12,28 @@ A docker I created for serving wordpress, opencart and magento behind a reverse 
 * Most php-modules are included.
 * Daily reload of configs when mod_ssl is enabled (for reloading e.g. letsencrypt certificates)
 * Automatic population of /etc/apache2 /etc/php and /etc/nullmailer if configs are not found (e.g. first run on mounted empty dir)
-* ENVIRONMENT: use MODE=mod for using mod_php instead of php-fpm. Apache2 will be set to mpm_prefork.
-* ENVIRONMENT: use CACHE=yes to enable caching
-* ENVIRONMENT: use TZ=Europe/Amsterdam (for example) to set timezone 
-* ENVIRONMENT: A2ENMOD A2DISMOD A2ENCONF A2DISCONF (changes are persistent if you mount /etc/apache2)
-* ENVIRONMENT: pre-enabled modules during build: proxy_fcgi setenvif rewrite expires headers remoteip
+* pre-enabled modules during build: proxy_fcgi setenvif rewrite expires headers remoteip
+
+ENVIRONMENT:
+* use CACHE=yes to enable caching
+* use TZ=Europe/Amsterdam (for example) to set timezone 
+* variables A2ENMOD A2DISMOD A2ENCONF A2DISCONF (changes are persistent if you mount /etc/apache2)
+* use MODE=mod for using mod_php instead of php-fpm. Apache2 will be set to mpm_prefork.
+* use MODE=multi to enable running multiple PHP versions. Need to pull the multi-tag docker
+* in multimode set environment PHP56=yes PHP72=yes PHP74=yes PHP80=yes to enable specific versions.
 
 See [docker-compose.yml](https://github.com/eilandert/dockerized/blob/master/apache-phpfpm/docker-compose.yml) for examples.
 
 ##### Tags:
+
+eilandert/apache-phpfpm:latest defaults to 7.4 at this time
 
 eilandert/apache-phpfpm:5.6<BR>
 eilandert/apache-phpfpm:7.2<BR>
 eilandert/apache-phpfpm:7.4<BR>
 eilandert/apache-phpfpm:8.0<BR>
 
-eilandert/apache-phpfpm:latest defaults to 7.4 at this time
+eilandert/apache-phpfpm:multi for all php versions
 
 ##### TODO
 
--- apache-phpfpm/bootstrap.sh
+++ apache-phpfpm/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-        echo "[APACHE-PHPFM-${PHPVERSION}] This docker image can be found on https://hub.docker.com/u/eilandert or https://github.com/eilandert/dockerized"
+        echo "[APACHE-PHPFM] This docker image can be found on https://hub.docker.com/u/eilandert or https://github.com/eilandert/dockerized"
 
         chmod 777 /dev/stdout
 
@@ -17,11 +17,13 @@
           cp -r /etc/apache2.orig/* /etc/apache2/
         fi
 
-        FIRSTRUN="/etc/php/${PHPVERSION}/fpm/php-fpm.conf"
-        if [ ! -f ${FIRSTRUN} ]; then
+	if [ ! "${MODE}" = "multi" ]; then
+          FIRSTRUN="/etc/php/${PHPVERSION}/fpm/php-fpm.conf"
+          if [ ! -f ${FIRSTRUN} ]; then
           echo "[APACHE-PHPFM] no configs found, populating default configs to /etc/php"
           cp -r /etc/php.orig/* /etc/php/
-        fi
+          fi
+	fi
 
         FIRSTRUN="/etc/nullmailer/defaultdomain"
         if [ ! -f ${FIRSTRUN} ]; then
@@ -36,6 +38,68 @@
         /bin/chown -R mail:mail /var/spool/nullmailer/ /etc/nullmailer
         runuser -u mail /usr/sbin/nullmailer-send 1>/var/log/nullmailer.log 2>&1 &
 
+        if [ "${MODE}" = "multi" ]; then
+          #fix some weird issue with php-fpm
+          if [ ! -x /run/php ]; then
+            mkdir -p /run/php
+            chown www-data:www-data /run/php
+            chmod 755 /run/php
+	  fi
+
+	  #make sure no php-fpm is running, enabling them later on request
+	  service php5.6-fpm stop 1>/dev/null 2>&1
+	  service php7.2-fpm stop 1>/dev/null 2>&1
+	  service php7.4-fpm stop 1>/dev/null 2>&1
+	  service php8.0-fpm stop 1>/dev/null 2>&1
+
+	  if [ "${PHP56}" = "yes" ]; then
+            FIRSTRUN="/etc/php/5.6/fpm/php-fpm.conf"
+            if [ ! -f ${FIRSTRUN} ]; then
+              echo "[APACHE-PHPFM] no configs found, populating default configs to /etc/php/5.6"
+	      mkdir -p /etc/php/5.6
+              cp -r /etc/php.orig/5.6/* /etc/php/5.6
+	    fi
+	    php-fpm5.6 -v
+	    php-fpm5.6 -t
+	    service php5.6-fpm restart 1>/dev/null 2>&1
+	  fi
+
+          if [ "${PHP72}" = "yes" ]; then
+            FIRSTRUN="/etc/php/7.2/fpm/php-fpm.conf"
+            if [ ! -f ${FIRSTRUN} ]; then
+              echo "[APACHE-PHPFM] no configs found, populating default configs to /etc/php/7.2"
+              mkdir -p /etc/php/7.2
+              cp -r /etc/php.orig/7.2/* /etc/php/7.2
+            fi
+            php-fpm7.2 -v
+            php-fpm7.2 -t
+            service php7.2-fpm restart 1>/dev/null 2>&1
+          fi
+
+          if [ "${PHP74}" = "yes" ]; then
+            FIRSTRUN="/etc/php/7.4/fpm/php-fpm.conf"
+            if [ ! -f ${FIRSTRUN} ]; then
+              echo "[APACHE-PHPFM] no configs found, populating default configs to /etc/php/7.4"
+              mkdir -p /etc/php/7.4
+              cp -r /etc/php.orig/7.4/* /etc/php/7.4
+            fi
+            php-fpm7.4 -v
+            php-fpm7.4 -t
+            service php7.4-fpm restart 1>/dev/null 2>&1
+          fi
+
+          if [ "${PHP80}" = "yes" ]; then
+            FIRSTRUN="/etc/php/8.0/fpm/php-fpm.conf"
+            if [ ! -f ${FIRSTRUN} ]; then
+              echo "[APACHE-PHPFM] no configs found, populating default configs to /etc/php/8.0"
+              mkdir -p /etc/php/8.0
+              cp -r /etc/php.orig/8.0/* /etc/php/8.0
+            fi
+            php-fpm8.0 -v
+            php-fpm8.0 -t
+            service php8.0-fpm restart 1>/dev/null 2>&1
+          fi
+	fi
 
 	if [ "${MODE}" = "fpm" ]; then
     	  #fix some weird issue with php-fpm
@@ -48,9 +112,9 @@
 	  a2enconf php${PHPVERSION}-fpm 1>/dev/null 2>&1
 	  a2dismod mpm_prefork 1>/dev/null 2>&1
 	  a2enmod mpm_event 1>/dev/null 2>&1
+          php-fpm${PHPVERSION} -v
+          php-fpm${PHPVERSION} -t
           service php${PHPVERSION}-fpm restart 1>/dev/null 2>&1
-	  php-fpm${PHPVERSION} -t
-	  php-fpm${PHPVERSION} -v
 	fi
 
         if [ "${MODE}" = "mod" ]; then
@@ -95,7 +159,6 @@
 	apachectl configtest
 
 	if [ -f /etc/apache2/mods-enabled/ssl.load ]; then
-	  echo "Automaticly reloading configs everyday to pick up new ssl certificates"
 	  while [ 1 ]; do sleep 1d; apachectl graceful; done &
 	fi
 
