45b38092b0796b8cfe133db6fdc842f11ba3901c
Thijs Eilander <eilander@myguard.nl>
2020-10-26 01:08:26 +0100

    roundup

--- nginx-proxy-modsecurity-pagespeed/Dockerfile.php
+++ nginx-proxy-modsecurity-pagespeed/Dockerfile.php
@@ -4,24 +4,24 @@ ENV	DEBIAN_FRONTEND="noninteractive"
 
 COPY    bootstrap-php.sh /bootstrap.sh
 
-RUN     UBUNTU=`lsb_release -c -s` \
-	&& echo "deb [trusted=yes] http://ppa.launchpad.net/eilander/nginx/ubuntu ${UBUNTU} main" > /etc/apt/sources.list.d/eilander-ubuntu-nginx-${UBUNTU}.list \
-        && echo "deb [trusted=yes] http://ppa.launchpad.net/ondrej/php/ubuntu ${UBUNTU} main" > /etc/apt/sources.list.d/ondrej-ubuntu-php-${UBUNTU}.list \
-	&& apt-get update && apt-get -y upgrade \
+RUN     . /etc/os-release \
+        && echo "deb [trusted=yes] http://ppa.launchpad.net/eilander/nginx/ubuntu ${UBUNTU_CODENAME} main" > /etc/apt/sources.list.d/eilander-ubuntu-nginx-${UBUNTU_CODENAME}.list \
+        && echo "deb [trusted=yes] http://ppa.launchpad.net/ondrej/php/ubuntu ${UBUNTU_CODENAME} main" > /etc/apt/sources.list.d/ondrej-ubuntu-php-${UBUNTU_CODENAME}.list \
+        && apt-get update && apt-get -y upgrade \
         && apt-get install -y nullmailer mailutils \
-	&& apt-get -y --no-install-recommends install \
-		nginx-full \ 
-		libmodsecurity3 \
-		libnginx-mod-modsecurity \
-		libnginx-mod-pagespeed \
-		libnginx-mod-security-headers \
-		libnginx-mod-brotli \
-		libnginx-mod-ssl-ct \
+        && apt-get -y --no-install-recommends install \
+                nginx-full \
+                libmodsecurity3 \
+                libnginx-mod-modsecurity \
+                libnginx-mod-pagespeed \
+                libnginx-mod-security-headers \
+                libnginx-mod-brotli \
+                libnginx-mod-ssl-ct \
                 modsecurity-crs \
-	&& apt-get -y autoremove && apt-get -y autoclean && rm -rf /var/lib/apt/lists/* \
-	&& mv /etc/nginx /etc/nginx.orig && mkdir -p /etc/nginx \
-	&& mv /etc/modsecurity /etc/modsecurity.orig && mkdir -p /etc/modsecurity \
-	&& chmod +x /bootstrap.sh 
+        && apt-get -y autoremove && apt-get -y autoclean && rm -rf /var/lib/apt/lists/* \
+        && mv /etc/nginx /etc/nginx.orig && mkdir -p /etc/nginx \
+        && mv /etc/modsecurity /etc/modsecurity.orig && mkdir -p /etc/modsecurity \
+        && chmod +x /bootstrap.sh
 
 ENV     PHPVERSION=5.6
 RUN     apt-get update && apt-get -y upgrade \
