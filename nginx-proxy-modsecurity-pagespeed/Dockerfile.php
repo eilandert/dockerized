414dfaa94ac8db4a44bd96b341187e83c12bce6c
Thijs Eilander <eilander@myguard.nl>
2020-11-13 00:57:21 +0100

    autogenerated dockerfiles

--- nginx-proxy-modsecurity-pagespeed/Dockerfile.php
+++ nginx-proxy-modsecurity-pagespeed/Dockerfile.php
@@ -1,14 +1,12 @@
-FROM	eilandert/ubuntu-base:rolling
-LABEL	maintainer="Thijs Eilander <eilander@myguard.nl>"
-ENV	DEBIAN_FRONTEND="noninteractive"
+FROM    eilandert/php-fpm:multi
+LABEL   maintainer="Thijs Eilander <eilander@myguard.nl>"
+ENV     DEBIAN_FRONTEND="noninteractive"
 
 COPY    bootstrap-php.sh /bootstrap.sh
 
 RUN     . /etc/os-release \
         && echo "deb [trusted=yes] http://ppa.launchpad.net/eilander/nginx/ubuntu ${UBUNTU_CODENAME} main" > /etc/apt/sources.list.d/eilander-ubuntu-nginx-${UBUNTU_CODENAME}.list \
-        && echo "deb [trusted=yes] http://ppa.launchpad.net/ondrej/php/ubuntu ${UBUNTU_CODENAME} main" > /etc/apt/sources.list.d/ondrej-ubuntu-php-${UBUNTU_CODENAME}.list \
         && apt-get update && apt-get -y upgrade \
-        && apt-get install -y nullmailer mailutils \
         && apt-get -y --no-install-recommends install \
                 nginx-full \
                 libmodsecurity3 \
@@ -23,98 +21,8 @@ RUN     . /etc/os-release \
         && mv /etc/modsecurity /etc/modsecurity.orig && mkdir -p /etc/modsecurity \
         && chmod +x /bootstrap.sh
 
-ENV     PHPVERSION=5.6
-RUN     apt-get update && apt-get -y upgrade \
-	&& apt-get install -y --no-install-recommends \
-                php${PHPVERSION} \
-                php${PHPVERSION}-fpm \
-                php${PHPVERSION}-apcu \
-                php${PHPVERSION}-bcmath \
-                php${PHPVERSION}-cli \
-                php${PHPVERSION}-curl \
-                php${PHPVERSION}-dom \
-                php${PHPVERSION}-gd \
-                php${PHPVERSION}-igbinary \
-                php${PHPVERSION}-imagick \
-                php${PHPVERSION}-json \
-                php${PHPVERSION}-memcached \
-                php${PHPVERSION}-mbstring \
-                php${PHPVERSION}-mcrypt \
-                php${PHPVERSION}-mysql \
-                php${PHPVERSION}-opcache \
-                php${PHPVERSION}-pgsql \
-                php${PHPVERSION}-readline \
-                php${PHPVERSION}-recode \
-                php${PHPVERSION}-redis \
-                php${PHPVERSION}-soap \
-                php${PHPVERSION}-tidy \
-                php${PHPVERSION}-xml \
-                php${PHPVERSION}-zip \
-        && apt-get -y autoremove && apt-get -y autoclean
 
-ENV     PHPVERSION=7.4
-RUN     apt-get update && apt-get -y upgrade \
-        && apt-get install -y --no-install-recommends \
-                php${PHPVERSION} \
-                php${PHPVERSION}-fpm \
-                php${PHPVERSION}-apcu \
-                php${PHPVERSION}-bcmath \
-                php${PHPVERSION}-cli \
-                php${PHPVERSION}-curl \
-                php${PHPVERSION}-dom \
-                php${PHPVERSION}-gd \
-                php${PHPVERSION}-igbinary \
-                php${PHPVERSION}-imagick \
-                php${PHPVERSION}-json \
-                php${PHPVERSION}-memcached \
-                php${PHPVERSION}-mbstring \
-#               php${PHPVERSION}-mcrypt \
-                php${PHPVERSION}-mysql \
-                php${PHPVERSION}-opcache \
-                php${PHPVERSION}-pgsql \
-                php${PHPVERSION}-readline \
-#               php${PHPVERSION}-recode \
-                php${PHPVERSION}-redis \
-                php${PHPVERSION}-soap \
-                php${PHPVERSION}-tidy \
-                php${PHPVERSION}-xml \
-                php${PHPVERSION}-zip \
-        && apt-get -y autoremove && apt-get -y autoclean
-
-ENV     PHPVERSION=8.0
-RUN     apt-get update && apt-get -y upgrade \
-        && apt-get install -y --no-install-recommends \
-                php${PHPVERSION} \
-                php${PHPVERSION}-fpm \
-                php${PHPVERSION}-apcu \
-                php${PHPVERSION}-bcmath \
-                php${PHPVERSION}-cli \
-                php${PHPVERSION}-curl \
-                php${PHPVERSION}-dom \
-                php${PHPVERSION}-gd \
-                php${PHPVERSION}-igbinary \
-                php${PHPVERSION}-imagick \
-#                php${PHPVERSION}-json \
-                php${PHPVERSION}-memcached \
-                php${PHPVERSION}-mbstring \
-#               php${PHPVERSION}-mcrypt \
-                php${PHPVERSION}-mysql \
-                php${PHPVERSION}-opcache \
-                php${PHPVERSION}-pgsql \
-                php${PHPVERSION}-readline \
-#               php${PHPVERSION}-recode \
-                php${PHPVERSION}-redis \
-                php${PHPVERSION}-soap \
-                php${PHPVERSION}-tidy \
-                php${PHPVERSION}-xml \
-                php${PHPVERSION}-zip \
-        && apt-get -y autoremove && apt-get -y autoclean
-
-RUN     mv /etc/php /etc/php.orig && mkdir -p /etc/php \
-        && mv /etc/nullmailer /etc/nullmailer.orig && mkdir -p /etc/nullmailer \
-        && apt-get -y autoremove && apt-get -y autoclean \
-        && rm -rf /var/lib/apt/lists/*
-
-CMD	["/bootstrap.sh"]
-EXPOSE	80 80
+CMD     ["/bootstrap.sh"]
+EXPOSE  80 80
 EXPOSE  443 443
+
