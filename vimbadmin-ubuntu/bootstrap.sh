b2ef75a2f0eaed450a3e75beb583cc2c71872cda
Thijs Eilander <eilander@myguard.nl>
2020-12-20 00:45:52 +0100

    move some stuff from bootstrap.sh to Dockerfile

--- vimbadmin-ubuntu/bootstrap.sh
+++ vimbadmin-ubuntu/bootstrap.sh
@@ -18,35 +18,31 @@ fi
 # If there are no configfiles, copy them
 FIRSTRUN="/etc/apache2/apache2.conf"
 if [ ! -f ${FIRSTRUN} ]; then
-    echo "[VIMBADMIN] apache: no configs found, populating default configs to /etc/apache2"
-    cp -r /etc/apache2.orig/* /etc/apache2/
+    #    echo "[VIMBADMIN] apache: no configs found, populating default configs to /etc/apache2"
+    cp -r /etc/apache2.orig/* /etc/apache2/ &
 fi
 
 FIRSTRUN="/etc/php/${PHPVERSION}/fpm/php-fpm.conf"
 if [ ! -f ${FIRSTRUN} ]; then
-    echo "[VIMBADMIN] php: no configs found, populating default configs to /etc/php/${PHPVERSION}"
+    #    echo "[VIMBADMIN] php: no configs found, populating default configs to /etc/php/${PHPVERSION}"
     mkdir -p /etc/php/${PHPVERSION}
-    cp -r /etc/php.orig/${PHPVERSION}/* /etc/php/${PHPVERSION}
+    cp -r /etc/php.orig/${PHPVERSION}/* /etc/php/${PHPVERSION} &
 fi
-
-service php${PHPVERSION}-fpm restart 1>/dev/null 2>&1
 
 FIRSTRUN="${INSTALL_PATH}/application/configs/application.ini"
 if [ ! -f ${FIRSTRUN} ]; then
     echo "[VIMBADMIN] vimbadmin: application.ini not found, populating default configs to ${INSTALL_PATH}/application/configs/"
     cp -rp ${INSTALL_PATH}/application/configs.orig/* ${INSTALL_PATH}/application/configs/
-    cp ${INSTALL_PATH}/application/configs/application.ini.dist ${INSTALL_PATH}/application/configs/application.ini
-    chown -R www-data:www-data ${INSTALL_PATH}/var
-    echo "[docker : production]" >> ${INSTALL_PATH}/application/configs/application.ini*
-
+    chown -R www-data:www-data ${INSTALL_PATH}/var &
 else
     # 4-6-2020, change existing application.ini after upgrade to 3.2.0, removal from this file far in future.
     sed -i 's~"/../vendor/opensolutions/oss-framework/src/OSS/Resource"~"/../library/OSS/Resource"~' ${INSTALL_PATH}/application/configs/application.ini
     sed -i 's~"/../vendor/opensolutions/oss-framework/src/OSS/Smarty/functions"~"/../library/OSS/Smarty/functions"~' ${INSTALL_PATH}/application/configs/application.ini
-
     # 17-11-2020 remove loadmodule in the sitesnippet (migrating from alpine to ubuntu)
     sed -i '/^LoadModule/d' /etc/apache2/sites-enabled/000-default.conf
 fi
+
+service php${PHPVERSION}-fpm restart 1>/dev/null 2>&1
 
 if [ -f /etc/apache2/mods-enabled/ssl.load ]; then
     while [ 1 ]; do sleep 1d; apachectl graceful; done &
@@ -57,4 +53,3 @@ if [ -f /run/apache2/apache2.pid ]; then
 fi
 
 exec /usr/sbin/apache2ctl -DFOREGROUND
-
