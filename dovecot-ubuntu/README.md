deef4e8f4b919225cb58c74803d61290f349e89a
Thijs Eilander <eilander@myguard.nl>
2020-12-23 01:52:29 +0100

    update

--- dovecot-ubuntu/README.md
+++ dovecot-ubuntu/README.md
@@ -1,10 +1,25 @@
 dockerized dovecot+sieve image based on ubuntu:rolling
 
-Mount /etc/dovecot for configfiles, if dovecot.conf does not exist all default configfiles will be copied/
-
-Build with dovecot packages on https://launchpad.net/~eilander/+archive/ubuntu/backports
-
+Mount /etc/dovecot for configfiles, if dovecot.conf does not exist all default configfiles will be copied on startup.
+Built with dovecot packages on https://launchpad.net/~eilander/+archive/ubuntu/backports
 The docker-compose file is on my github https://github.com/eilandert/dockerized/blob/master/dovecot-ubuntu/docker-compose.yml
 
 Environment variable(s):
-SYSLOG_HOST
+  SYSLOG_HOST=10.0.0.1
+  TZ=Europe/Amsterdam
+  DB_DRIVER=mysql
+  DB_DATABASE=vimbadmin
+  DB_HOST=172.30.0.100
+  DB_PORT=3306
+  DB_USERNAME=vimbadmin
+  DB_PASSWORD=
+
+This docker is loaded with:
+  Dovecot and all modules (obviously)
+  rspamd (to use rspamd's client rspamc)
+  spamc (spamassassin client)
+  pyzor 
+  razor 
+  vimbadmin scripts in /opt/scripts/vimbadmin
+
+Drivers for pgsql are also present.
--- dovecot-ubuntu/bootstrap.sh
+++ dovecot-ubuntu/bootstrap.sh
@@ -24,10 +24,21 @@ else
     rm -f /etc/syslog-ng/conf.d/remote.conf
 fi
 
-if [ -n "${DB_DRIVER}" ]; then
-    sed -i s/"driver = .*"/"driver = ${DB_DRIVER}"/ /etc/dovecot/dovecot-sql.conf.ext
-    sed -i s/"connect = host=localhost user=vimbadmin password=password dbname=vimbadmin"/"connect = host=${DB_HOST} user=${DB_USERNAME} password=${DB_PASSWORD} dbname=${DB_DATABASE}"/ /etc/dovecot/dovecot-sql.conf.ext
-fi
+#if [ -n "${DB_DRIVER}" ]; then
+#    sed -i s/"driver = .*"/"driver = ${DB_DRIVER}"/ /etc/dovecot/dovecot-sql.conf.ext
+#    sed -i s/"connect = host=localhost user=vimbadmin password=password dbname=vimbadmin"/"connect = host=${DB_HOST} user=${DB_USERNAME} password=${DB_PASSWORD} dbname=${DB_DATABASE}"/ /etc/dovecot/dovecot-sql.conf.ext
+#fi
+
+if [ -n "${USE_VIMBADMIN}" ]; then
+    cp -rp /opt/scripts/vimbadmin.orig /opt/scripts/vimbadmin
+    sed -i s/"my \$driver .*"/"my \$driver   = \"${DB_DRIVER}\";"/ /opt/scripts/vimbadmin/*
+    sed -i s/"my \$database .*"/"my \$database = \"${DB_DATABASE}\";"/ /opt/scripts/vimbadmin/*
+    sed -i s/"my \$host .*"/"my \$host = \"${DB_HOST}\";"/ /opt/scripts/vimbadmin/*
+    sed -i s/"my \$port .*"/"my \$port = \"${DB_PORT}\";"/ /opt/scripts/vimbadmin/*
+    sed -i s/"my \$username .*"/"my \$username = \"${DB_USERNAME}\";"/ /opt/scripts/vimbadmin/*
+    sed -i s/"my \$password .*"/"my \$password = \"${DB_PASSWORD}\";"/ /opt/scripts/vimbadmin/*
+fi &
+
 
 chmod 777 /dev/stdout
 
