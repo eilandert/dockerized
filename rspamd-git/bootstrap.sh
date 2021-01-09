c2aa325ca5333e0a168ee4b087e036ed4c6f8b59
Thijs Eilander <eilander@myguard.nl>
2021-01-09 23:47:28 +0100

    change path from /usr/local/etc to /etc

--- rspamd-git/bootstrap.sh
+++ rspamd-git/bootstrap.sh
@@ -2,11 +2,19 @@
 
 echo "[RSPAMD] This docker image can be found on https://hub.docker.com/u/eilandert and https://github.com/eilandert/dockerized"
 
-FIRSTRUN="/usr/local/etc/rspamd/rspamd.conf"
+CHECK="/usr/local/etc/rspamd/rspamd.conf"
+if [ -f ${CHECK} ]; then
+
+	echo "YOU HAVE MOUNTED CONFIGS TO /USR/LOCAL/ETC. THIS HAS CHANGED TO /ETC"
+	echo "PLEASE CHANGE YOUR MOUNTS INSIDE THE CONTAINER ACCORDINGLY"
+	exit
+fi
+
+FIRSTRUN="/etc/rspamd/rspamd.conf"
 if [ ! -f ${FIRSTRUN} ]; then
     echo "[RSPAMD] no configs found, populating default configs to /usr/local/etc/rspamd"
-    mkdir -p /usr/local/etc/rspamd
-    cp -r /usr/local/etc/rspamd.orig/* /usr/local/etc/rspamd/
+    mkdir -p /usr/etc/rspamd
+    cp -r /etc/rspamd.orig/* /etc/rspamd/
 
     mkdir -p /var/log/rspamd
     mkdir -p /var/lib/rspamd
@@ -15,7 +23,6 @@ if [ ! -f ${FIRSTRUN} ]; then
     chown _rspamd:_rspamd -R /var/log/rspamd
     chown _rspamd:_rspamd -R /var/lib/rspamd
     chown _rspamd:_rspamd -R /var/run/rspamd
-
 fi
 
 if [ -n "${TZ}" ]; then
@@ -30,10 +37,10 @@ if [ -n "${SYSLOG_HOST}" ]; then
     echo "destination dst { syslog(\"${SYSLOG_HOST}\" transport(\"udp\")); };" > /etc/syslog-ng/conf.d/remote.conf
     echo "log { source(s_sys); destination(dst); };" >> /etc/syslog-ng/conf.d/remote.conf
     syslog-ng --no-caps
-    echo "type = \"syslog\";" > /usr/local/etc/rspamd/override.d/logging.inc
+    echo "type = \"syslog\";" > /etc/rspamd/override.d/logging.inc
     echo "[RSPAMD] Output is set to remote syslog at ${SYSLOG_HOST}"
 else
-    echo "type = \"console\";" > /usr/local/etc/rspamd/override.d/logging.inc
+    echo "type = \"console\";" > /etc/rspamd/override.d/logging.inc
 fi
 
 exec	/usr/local/bin/rspamd -f -u _rspamd -g _rspamd;
