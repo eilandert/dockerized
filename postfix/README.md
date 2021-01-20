28a9070819ae6ee6fb71a7dc06c670d052b3af3b
Thijs Eilander <eilander@myguard.nl>
2021-01-20 22:42:27 +0100

    change from alpine to ubuntu:rolling with packages from deb.paranoid.nl

--- postfix/README.md
+++ postfix/README.md
@@ -1,8 +1,9 @@
-dockerized postfix based on alpine:edge
-
-added: /etc/postfix/cron.d/ for reload and stuff
+dockerized postfix based on ubuntu:rolling with packages from http://deb.paranoid.nl
 
 If /etc/postfix/main.cf does not exist, all the original configurationfiles will be copied to /etc/postfix
 
 Environment variable(s):
 SYSLOG_HOST
+TZ
+
+Daily Reload to pick up SSL Certificate changes
--- postfix/bootstrap.sh
+++ postfix/bootstrap.sh
@@ -2,28 +2,37 @@
 
 echo "[POSTFIX] This docker image can be found on https://hub.docker.com/u/eilandert and https://github.com/eilandert/dockerized"
 
+if [ -n "${TZ}" ]; then
+    rm -f /etc/timezone /etc/localtime
+    echo "${TZ}" > /etc/timezone
+    ln -s /usr/share/zoneinfo/${TZ} /etc/localtime
+fi
+
 FIRSTRUN="/etc/postfix/main.cf"
 if [ ! -f ${FIRSTRUN} ]; then
     echo "[POSTFIX] main.cf not found, populating default configs to /etc/postfix"
-    mkdir -p /etc/postfix \
-        && cp -r /etc/postfix.orig/* /etc/postfix/
-fi
-
-if [ -n "${TZ}" ]; then
-    echo "${TZ}" > /etc/timezone
+    mkdir -p /etc/postfix
+    cp -r /etc/postfix.orig/* /etc/postfix/
 fi
 
 if [ -n "${SYSLOG_HOST}" ]; then
+    mkdir -p /etc/syslog-ng/conf.d
     echo "destination dst { syslog(\"${SYSLOG_HOST}\" transport(\"udp\")); };" > /etc/syslog-ng/conf.d/remote.conf
     echo "log { source(s_sys); destination(dst); };" >> /etc/syslog-ng/conf.d/remote.conf
-    syslog-ng
+    syslog-ng --no-caps
     postconf -# maillog_file
     echo "[POSTFIX] Output is set to remote syslog at ${SYSLOG_HOST}"
 else
+    rm -f /etc/syslog-ng/conf.d/remote.conf
     postconf maillog_file=/dev/stdout
 fi
 
 chown postfix:postfix -R /var/lib/postfix
-chmod 777 /dev/stdout
+
+#echo "Automaticly reloading configs everyday to pick up new ssl certificates"
+while [ 1 ]; do sleep 1d; postfix reload; done &
+sleep 60 && postfix flush &
+
+echo -n "Postfix "; postconf mail_version | cut -d" " -f3
 
 exec postfix start-fg
