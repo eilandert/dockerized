7e1edf08967e5feb546d9c7f177a25edec99213a
root <you@example.com>
2021-05-24 18:12:38 +0200

    change to ubuntu as base image

--- clamav-unofficial-signatures/bootstrap.sh
+++ clamav-unofficial-signatures/bootstrap.sh
@@ -2,42 +2,55 @@
 
 echo "[CLAMAV] This docker image can be found on https://hub.docker.com/u/eilandert and https://github.com/eilandert/dockerized"
 
+CHECK="/config"
+if [ -r ${CHECK} ]; then
+    rm -rf /etc/clamav /etc/clamav-unofficial-sigs
+    ln -s /config/clamav /etc/clamav
+    ln -s /config/clamav-unofficial-sigs /etc/clamav-unofficial-sigs
+fi
+
 # If there are no configfiles, copy them
-FIRSTRUN="/config/clamav/clamd.conf"
+FIRSTRUN="/etc/clamav/clamd.conf"
 if [ ! -f ${FIRSTRUN} ]; then
     echo "[CLAMAV] clamd.conf not found, populating default configs to /config"
-    mkdir -p /config \
-        && cp -r config.orig/* /config/
+    cp -r config.orig/* /etc
 fi
+
+echo -n "[CLAMAV] Running: "
+clamd --version
 
 if [ -n "${TZ}" ]; then
     echo "${TZ}" > /etc/timezone
 fi
 
-# Make symlinks to /config so we can bind to /config
-rm -rf /etc/clamav && ln -s /config/clamav /etc/clamav
-rm -rf /etc/clamav-unofficial-sigs && ln -s /config/clamav-unofficial-sigs/ /etc/clamav-unofficial-sigs
-
-# copy updated configfiles
-cp /config.orig/clamav-unofficial-sigs/master.conf /config/clamav-unofficial-sigs/master.conf
-cp /config.orig/clamav-unofficial-sigs/user.conf /config/clamav-unofficial-sigs/user.conf
+echo "[CLAMAV] Updating the unofficial signatures updater from https://github.com/extremeshok/clamav-unofficial-sigs"
+clamav-unofficial-sigs.sh --upgrade 1>/dev/null 2>&1
 
 # make stdout wordwriteable for docker console output
 chmod 777 /dev/stdout
+mkdir -p /var/run/clamav
+chown clamav:clamav -R /var/run/clamav
+chown clamav:clamav -R /var/lib/clamav
 
 #Are there signatures?
 CVD_FILE="/var/lib/clamav/main.cvd"
 if [ ! -f ${CVD_FILE} ]; then
-    echo "[CLAMAV] main.cvd not found"
-    echo "[CLAMAV] Running Freshclam in foreground for one time only. This can take a while."
+    echo "[CLAMAV] main.cvd not found, assuming there are no signatures..."
+    echo "[CLAMAV] Running updaters in foreground to get signatures. This can take a while."
     freshclam --user=clamav --no-warnings --foreground
 fi
 
+echo "[CLAMAV] Starting ClamAV-milter"
+clamav-milter
+
 #poor mans cron
 echo "[CLAMAV] Starting updaters in the background"
-while [ 1 ]; do /usr/local/sbin/clamav-unofficial-sigs -s ; sleep 3661; done &
-freshclam -d -c6 --user=clamav
+while [ 1 ]; do 
+  /usr/local/sbin/clamav-unofficial-sigs.sh -s 1>/dev/null
+  sleep 3600
+done &
+freshclam -d
 
 echo "[CLAMAV] Starting clamd... Please wait while loading databases"
 
-exec   /usr/sbin/clamd
+exec   /usr/sbin/clamd --foreground
