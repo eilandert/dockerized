bb3a56d92cdd7f2de176fff16835a31dc7f95397
root <you@example.com>
2021-05-24 22:37:33 +0200

    updated the dummy bootstrap ;-)

--- base/bootstrap.sh
+++ base/bootstrap.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-echo "dummy bootstrap"
-
-sleep 1d;
+echo "---------------------------------------------------------------------------"
+echo "this is a dummy bootstrap, this is a base docker and does nothing right now"
+echo "---------------------------------------------------------------------------"
+echo "This docker can be found on https://hub.docker.com/u/eilandert and"
+echo "https://github.com/eilandert/dockerized"
+echo "It might include updated packages from http://deb.paranoid.nl"
+echo "Suggestions are welcome"
+echo "This docker will sleep now for 1h before exit"
+echo "---------------------------------------------------------------------------"
+sleep 1h;
