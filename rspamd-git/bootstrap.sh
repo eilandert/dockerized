9997f6925e33160163b43c2cf6b25ce0c72584d6
Thijs Eilander <eilander@myguard.nl>
2021-06-16 00:30:19 +0200

    exit the wait-for-it loop after x retries

--- rspamd-git/bootstrap.sh
+++ rspamd-git/bootstrap.sh
@@ -31,7 +31,7 @@ if [ -n "${TZ}" ]; then
     ln -s /usr/share/zoneinfo/${TZ} /etc/localtime
 fi
 
-mkdir -p /usr/local/etc/rspamd/override.d
+mkdir -p /etc/rspamd/override.d
 if [ -n "${SYSLOG_HOST}" ]; then
     mkdir -p /etc/syslog-ng/conf.d
     echo "destination dst { syslog(\"${SYSLOG_HOST}\" transport(\"udp\")); };" > /etc/syslog-ng/conf.d/remote.conf
@@ -48,21 +48,25 @@ sleep 1;
 
 # test services
 i=0
-while [ 1 ]
-do
+x=10
+while [ 1 ]; do
     i=$(($i+1))
     HOST=$(eval echo \$WAIT_FOR_$i)
     if [ ! -n "${HOST}" ]; then
         break;
     fi
-
-    /wait-for-it.sh ${HOST} -t 10
-
+    /wait-for-it.sh ${HOST} -t 3
     if [ "$?" -ne 0 ]; then
-        echo "... not reachable, trying again"
+        echo "... ${HOST} is not reachable, trying again"
         i=$(($i-1))
+        x=$(($x-1))
+        if [ "${x}" -eq 0 ]; then
+            echo "[RSPAMD] Nevermind, this is not going to work out! Goodbye!"
+            exit 255;
+        fi
     fi
 done
 
+echo "[RSPAMD] Starting rspamd..."
 
 exec	/usr/bin/rspamd -f -u _rspamd -g _rspamd;
