3d1ab18b9fe81728970658faa99e8a92046c2316
Thijs Eilander <eilander@myguard.nl>
2021-09-06 21:52:15 +0200

    fix syslog

--- redis/bootstrap.sh
+++ redis/bootstrap.sh
@@ -19,9 +19,11 @@ if [ -n "${SYSLOG_HOST}" ]; then
     mkdir -p /etc/syslog-ng/conf.d
     echo "destination dst { syslog(\"${SYSLOG_HOST}\" transport(\"udp\")); };" > /etc/syslog-ng/conf.d/remote.conf
     echo "log { source(s_sys); destination(dst); };" >> /etc/syslog-ng/conf.d/remote.conf
+    sed -i s/"^# syslog-enabled no"/"syslog-enabled yes/" /etc/redis/redis.conf
     syslog-ng --no-caps
     echo "[REDIS] Output is set to remote syslog at ${SYSLOG_HOST}"
 else
+    sed -i s/"^syslog-enabled yes/# syslog-enabled no"/ /etc/redis/redis.conf
     rm -f /etc/syslog-ng/conf.d/remote.conf
 fi
 
