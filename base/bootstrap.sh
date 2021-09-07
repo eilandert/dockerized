c8934467c6906aecbedc0adacb9dfd2a8c307673
Thijs Eilander <eilander@myguard.nl>
2021-09-07 02:43:23 +0200

    nicer logging

--- base/bootstrap.sh
+++ base/bootstrap.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+. /etc/os-release
+
 echo "---------------------------------------------------------------------------"
 echo "this is a dummy bootstrap, this is a base docker and does nothing right now"
 echo "---------------------------------------------------------------------------"
-echo "This docker can be found on https://hub.docker.com/u/eilandert and"
-echo "https://github.com/eilandert/dockerized"
-echo "It might include updated packages from http://deb.paranoid.nl"
-echo "Suggestions are welcome"
+echo "      This docker can be found on https://hub.docker.com/u/eilandert"
+echo "               and https://github.com/eilandert/dockerized"
+echo "             Packages can be found on https://deb.paranoid.nl"
+echo "---------------------------------------------------------------------------"
+echo "Running on ${PRETTY_NAME}"
+echo "---------------------------------------------------------------------------"
 echo "This docker will sleep now for 1h before exit"
 echo "---------------------------------------------------------------------------"
 sleep 1h;
+
