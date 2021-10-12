9bf3d5b527c2ddc466c814ae2d89b3f793cc9476
Thijs Eilander <eilander@myguard.nl>
2021-10-13 00:58:27 +0200

    renamed buildx.hcl to docker-bake.hcl

--- buildx.sh
+++ buildx.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+# This script uses buildx
+# To use/update buildx
+# git clone https://github.com/docker/buildx && cd buildx && make install
+
+
 # Ensure the push is only initiated on my build machine
 if [ "$(uname -n)" == "build" ]; then PUSH="--push"; fi
 
+#generate dockerfiles and prepare other things
 ./generate.sh
 
 docker buildx create --use
 
 for BUILD in base-current misc db mail phpfpm nginx apache apache-misc
 do
-    docker buildx bake ${PUSH} -f buildx.hcl ${BUILD}
+    docker buildx bake ${PUSH} ${BUILD}
 done
 
 docker buildx rm
