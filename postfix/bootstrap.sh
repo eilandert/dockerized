43c176f519f31e639d3d2efbc7f926f5b3dfcdff
Thijs Eilander <eilander@myguard.nl>
2021-11-27 01:31:55 +0000

    install jemalloc and mimalloc, make mimalloc default

--- postfix/bootstrap.sh
+++ postfix/bootstrap.sh
@@ -6,14 +6,11 @@ case ${MALLOC} in
     jemalloc)
         export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libjemalloc.so.2
         ;;
-    mimalloc)
+    *|mimalloc)
         export LD_PRELOAD=/usr/lib/mimalloc-2.0/libmimalloc-secure-none.so.2.0
         ;;
     none)
         unset LD_PRELOAD
-        ;;
-    *)
-        export LD_PRELOAD=/usr/lib/mimalloc-2.0/libmimalloc-secure-none.so.2.0
         ;;
 esac
 
