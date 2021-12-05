6460617f887d305e7b241b86cc824db83d3eb44d
Thijs Eilander <eilander@myguard.nl>
2021-12-05 15:04:18 +0000

    changed wait time to 1D

--- base/bootstrap.sh
+++ base/bootstrap.sh
@@ -1,17 +1,9 @@
 #!/bin/sh
 
-. /etc/os-release
-
 echo "---------------------------------------------------------------------------"
 echo "this is a dummy bootstrap, this is a base docker and does nothing right now"
 echo "---------------------------------------------------------------------------"
 echo "      This docker can be found on https://hub.docker.com/u/eilandert"
 echo "               and https://github.com/eilandert/dockerized"
-echo "             Packages can be found on https://deb.paranoid.nl"
-echo "---------------------------------------------------------------------------"
-echo "Running on ${PRETTY_NAME}"
-echo "---------------------------------------------------------------------------"
-echo "This docker will sleep now for 1h before exit"
-echo "---------------------------------------------------------------------------"
-sleep 1h;
+sleep 1d
 
