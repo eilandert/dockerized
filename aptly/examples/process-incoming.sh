132840e3f5a9d2f2651e0ff50f50db82e4727001
Thijs Eilander <eilander@myguard.nl>
2021-12-24 01:39:42 +0100

    improved the incoming processor example script

--- aptly/examples/process-incoming.sh
+++ aptly/examples/process-incoming.sh
@@ -1,26 +1,51 @@
 #!/bin/bash
 
-REPO=$1
-CREATE=$2
+#
+# Simple example script to process incoming packages, with filesystem endpoints
+# 20211224 Thijs Eilander <eilander@myguard.nl>
+#
 
-if [ -n "${CREATE}" ]; then
-  aptly repo create -distribution=${REPO} -component=main ${REPO}
+# You should put the files in /aptly/incoming and call this script
+#
+# Example:
+# scp *.deb *.changes *buildinfo aptly@192.168.178.2:/aptly/incoming
+# ssh aptly@192.168.178.2 "/aptly/scripts/process-incoming.sh bullseye bullseye-myownupdates CREATE"
+#
+# With the CREATE command the bullseye-myownupdates repo will automaticly be created and published if it does not exist yet
+#
+# Remove (or replace) the filesystem:${REPO}:. parts if you don't use filesystem endpoints in your .aptly.conf
+#
+
+DIST=$1
+REPO=$2
+ARG3=$3
+
+if [ -n "${ARG3}" = "CREATE" ]; then
+    CREATE="YES"
 fi
 
-cd /aptly/incoming
-for DEB in $(ls *deb)
-do
-  DEB=$(echo ${DEB}|sed s/_.*$//);
-  aptly repo remove ${REPO} ${DEB}
-done
+# When create flag is given: if repo doesn't exist, create it silently
+if [ -n "${CREATE}" ]; then
+    aptly repo create -distribution=${DIST} -component=main ${REPO} 1>/dev/null 2>&1
+fi
 
+# Remove all existing packages first before adding new
+cd /aptly/incoming
+DEB=$(ls *.deb | sed 's/_\S*//g')
+aptly repo remove ${REPO} ${DEB}
+aptly -architectures=amd64,i386,source,all publish update ${DIST} filesystem:${REPO}:.
+
+# Add everything from the .changes file
 aptly -architectures=amd64,i386,source,all -repo="${REPO}" repo include /aptly/incoming
 
+# When create flag is given: if repo is not published at all, publish it
 if [ -n "${CREATE}" ]; then
-  aptly -architectures=amd64,i386,source,all publish repo ${REPO}
+    aptly -architectures=amd64,i386,source,all publish repo ${REPO} filesystem:${REPO}:. 1>/dev/null 2>&1
 fi
 
-aptly -architectures=amd64,i386,source,all publish update ${REPO}
+# Update repo
+aptly -architectures=amd64,i386,source,all publish update ${DIST} filesystem:${REPO}:.
 
+# to be sure, empty incoming dir
 rm -f /aptly/incoming/*
 
