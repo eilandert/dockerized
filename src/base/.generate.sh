4296b49c1676f4f413ed3b70d0461b31cbdc7b1f
Build System <eilander@myguard.nl>
2026-05-16 00:51:15 +0200

    autogenerated: dockerfile generation from templates

--- src/base/.generate.sh
+++ src/base/.generate.sh
@@ -1,36 +1,29 @@
 #!/bin/bash
 # Generate base image Dockerfiles
 # Usage: base/.generate.sh
+# Sources config.sh for UBUNTULTS and DEBIANSTABLE versions
 
 set -e
 
 SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPTDIR"
 source ../../build/generate-lib.sh
+source ../../build/config.sh
 
 TEMPLATE="Dockerfile-template"
 check_template "$TEMPLATE" || exit 1
 
 log_info "Generating base image Dockerfiles..."
+log_info "Using config: UBUNTULTS=$UBUNTULTS DEBIANSTABLE=$DEBIANSTABLE"
 
 # Define variants: variant_name FROM DIST
+# Only generate primary Ubuntu and Debian versions from config
 declare -A VARIANTS=(
-    [devel]="ubuntu:devel|devel"
-    [rolling]="ubuntu:noble|noble"
-    [noble]="ubuntu:noble|noble"
-    [jammy]="ubuntu:jammy|jammy"
-    [focal]="ubuntu:focal|focal"
-    [bionic]="ubuntu:bionic|bionic"
-    [xenial]="ubuntu:xenial|xenial"
-    [trusty]="ubuntu:trusty|trusty"
-    [resolute]="ubuntu:resolute|resolute"
-    [trixie]="debian:trixie-slim|trixie"
-    [bookworm]="debian:bookworm-slim|bookworm"
-    [bullseye]="debian:bullseye-slim|bullseye"
-    [buster]="debian:buster-slim|buster"
-    [stretch]="debian:stretch-slim|stretch"
-    [jessie]="debian:jessie-slim|jessie"
+    [ubuntu-base]="ubuntu:${UBUNTULTS}|${UBUNTULTS}"
+    [debian-base]="debian:${DEBIANSTABLE}-slim|${DEBIANSTABLE}"
 )
+
+log_info "Generating ${#VARIANTS[@]} base image variants"
 
 for variant in "${!VARIANTS[@]}"; do
     IFS='|' read -r from dist <<< "${VARIANTS[$variant]}"
